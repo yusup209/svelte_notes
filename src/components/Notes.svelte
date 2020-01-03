95514f1392f7298af9cc0c98e5c655dadeb51da1
Muhammad Yusup <yusup.muhammad209@gmail.com>
2020-01-03 22:04:55 +0700

    sedikit perubahan

--- src/components/Notes.svelte
+++ src/components/Notes.svelte
@@ -1,14 +1,25 @@
 <script>
   import { db } from "../configs/firestore";
-  import Note from './Note.svelte';
+  import Note from "./Note.svelte";
+  import { onMount } from "svelte";
 
   let notes = [];
+  let done = false
 
-  db.collection("notes").orderBy('title').onSnapshot(data => {
-    notes = data.docs;
+  onMount(async () => {
+    await db.collection("notes")
+      .orderBy("title")
+      .onSnapshot(data => {
+        notes = data.docs;
+        done = true
+      });
   });
 </script>
 
-{#each notes as note}
-  <Note id={note.id} note={note.data()} />
-{/each}
+{#if notes.length === 0 && done == false}
+  <p style="font-style=italic;">loading...</p>
+{:else}
+  {#each notes as note}
+    <Note id={note.id} note={note.data()} />
+  {/each}
+{/if}
